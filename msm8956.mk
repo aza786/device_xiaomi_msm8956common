ba0fc3f921d87c2974400babdc5591afa6193612
aranhid <vladmir909@mail.ru>
2019-11-11 01:22:13 +0800

    msm8956-common: Fix mobile network
    Change-Id: I7348a9c581f9ec2259a4f0b6429a195d61734a78

--- msm8956.mk
+++ msm8956.mk
@@ -247,7 +247,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libbase_shim \
     ims-ext-common \
-    ims-ext-common_system
+    ims-ext-common_system \
+    android.hardware.radio@1.0 \
+    android.hardware.radio.config@1.0 \
+    android.hardware.secure_element@1.0
 
 PRODUCT_BOOT_JARS += \
     ims-ext-common_system
