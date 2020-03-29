837eaf77ea005c9906a0cb6df3dbdc318809a9c1
Magicxavi <rolfpeterkesche@gmail.com>
2020-03-29 18:10:49 +0300

    msm8956-common: Add touch HAL

--- msm8956.mk
+++ msm8956.mk
@@ -312,6 +312,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.aw2013
 
+# Lineage hardware
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.msm8956
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
